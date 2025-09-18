ecbd9420320a5493f2896605309d161d6a0cec67
andre.beaud@ing.com <un16fl@vienna-australiaeast-ip-ingress-nginx-01.australiaeast.cloudapp.azure.com>
2025-09-18 14:27:33 +1000

    Two

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Quick Start
 
 A detailed explanation on the usage of this repository can be found on this [Medium Article.](https://frank-ceballos.medium.com/deploying-your-first-fastapi-application-in-openshift-857cee7277f9?sk=33b2fb04e44d7098725e7ae2428468bf)
+
+PS: Trifling Change
--- main.py
+++ main.py
@@ -5,11 +5,19 @@ app = FastAPI()
 
 @app.get('/')
 def index():
+    return {'data': 'FastAPI is great!'}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8080)
     return {'data': 'Hello FastAPI!'}
 
 @app.get('/message')
 def index():
     return {'data': 'FastAPI is great!'}
+
+@app.get('/two')
+def index():
+    return {'data': '1 + 1'}
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8080)
